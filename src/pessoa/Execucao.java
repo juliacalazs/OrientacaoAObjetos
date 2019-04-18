cc08b4e975ba0bc75138e435a485852182c2ff84
juliacalazs <juliacalazans7@hotmail.com>
2019-04-18 10:44:32 -0300

    exercicio

---
+++ src/pessoa/Execucao.java
@@ -0,0 +1,17 @@
+package pessoa;
+
+import pessoa.Pessoa;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoa pes = new Pessoa();
+		String an=null, res=null, dor=null;
+
+		System.out.println(pes.andar(an));
+		System.out.println(pes.respirar(res));
+//		System.out.println(pes.dormir(dor));
+	}
+
+}
---
+++ src/pessoa/Pessoa.java
@@ -0,0 +1,21 @@
+package pessoa;
+
+public class Pessoa {
+	
+	public String andar(String and) {
+		and = "andando";
+		return and;
+	}
+	
+	protected String respirar(String res) {
+		res = "respirando";
+		return res;
+	}
+	
+	 private String dormir (String dor) {
+		 dor = "dormindo";
+		return dor;
+	}
+	
+
+}
